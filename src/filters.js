9a79ddffcfe5962cd05c11fbb55471bd1eae0a58
GiuliaP95 <kirakira95@msn.com>
2022-06-07 21:11:03 +0200

    Finished styling. To do: - checkbox for health labels; - add reset button; - add max number of results per page!!!!! - check again first api video and see what else they have?

--- src/filters.js
+++ src/filters.js
@@ -3,7 +3,7 @@ const filters={
         '','balanced','high-fiber','high-protein','low-carb','low-fat','low-sodium'
     ],
     health:[
-        '','alcohol-free','dairy-free','egg-free','fish-free','fodmap-free','gluten-free','immuno-supportive','low-sugar','Mediterranean','no-oil-added','paleo','peanut-free','pescatarian','soy-free','wheat-free'
+        '','alcohol-free','dairy-free','egg-free','fish-free','fodmap-free','gluten-free','immuno-supportive','low-sugar','Mediterranean','no-oil-added','paleo','peanut-free','pescatarian','soy-free','vegan','vegetarian','wheat-free'
     ],
     meal:[
         '','breakfast','lunch','dinner','snack','teatime'
